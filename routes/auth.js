c0ba393893c57c3b369e8bd2d8684c8c3666b779
arbajit0901 <arbajitsen0901@gmail.com>
2024-06-22 17:17:05 +0530

    test3

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const convertionController = require("../controllers/convertionController");
 
-router.get("/", convertionController.)
+router.get("/", convertionController.home);
 router.post("/celciustofahrenheit", convertionController.celciusToFahrenheit);
 
 module.exports = router;
